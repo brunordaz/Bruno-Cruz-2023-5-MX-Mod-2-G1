c1d73848b23661003fe52eefb6e5121c86131340
brunordaz <126126201+brunordaz@users.noreply.github.com>
2023-06-20 20:22:40 -0600

    Update TAREA 3

--- game/utils/constants.py
+++ game/utils/constants.py
@@ -1,6 +1,6 @@
 import pygame
 import os
-
+pygame.mixer.init()
 # Global Constants
 TITLE = "Spaceships Game"
 SCREEN_HEIGHT = 600
@@ -9,7 +9,7 @@ FPS = 30
 IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 
 # Assets Constants
-ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
+ICON = pygame.image.load(os.path.join(IMG_DIR, 'Spaceship/spaceship.png'))
 
 SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
 
@@ -20,12 +20,12 @@ HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
 DEFAULT_TYPE = "default"
 SHIELD_TYPE = 'shield'
 
-SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
-SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png"))
-BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
+SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "spaceship/spaceship.png"))
+SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "spaceship/spaceship_shield.png"))
+BULLET = pygame.image.load(os.path.join(IMG_DIR, "bullet/bullet_1.png"))
 
-BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
-ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
-ENEMY_2 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_2.png"))
+BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "bullet/bullet_2.png"))
+ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "enemy/enemy_1.png"))
+ENEMY_2 = pygame.image.load(os.path.join(IMG_DIR, "enemy/enemy_2.png"))
 
 FONT_STYLE = 'freesansbold.ttf'
