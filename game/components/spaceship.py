876ba90ce7afc080d678adfb453cf46e265fe79e
brunordaz <126126201+brunordaz@users.noreply.github.com>
2023-06-21 19:21:42 -0600

    Update spaceship.py

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,43 +1,66 @@
 import pygame
-
 from pygame.sprite import Sprite
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, BULLET
 
-from game.utils.constants import SPACESHIP,SCREEN_WIDTH,SCREEN_HEIGHT
-#sprite es un objeto de pygame
+class Bullet(Sprite):
+    def __init__(self, position):
+        super().__init__()
+        self.image = pygame.transform.scale(BULLET, (20, 20))
+        self.rect = self.image.get_rect()
+        self.rect.x = position[0]
+        self.rect.y = position[1]
+
+    def update(self):
+        self.rect.y -= 8
+
 class Spaceship(Sprite):
     def __init__(self):
         self.image = SPACESHIP
-        self.image = pygame.transform.scale(self.image,(50,60)) #ancho y alto
+        self.image = pygame.transform.scale(self.image, (50, 60))
         self.rect = self.image.get_rect()
-        self.speed = 8  #Movimiento de la nave, velocidad de desplazamiento
-        self.rect.x = (SCREEN_WIDTH - self.rect.width) // 2 #Coloca la nave en el centro del eje x
-        self.rect.y = (SCREEN_HEIGHT - self.rect.height) #Coloca la nave en el tope inferior de la pantalla en el eje y
+        self.speed = 8
+        self.rect.x = (SCREEN_WIDTH - self.rect.width) // 2
+        self.rect.y = SCREEN_HEIGHT - self.rect.height
+        self.bullets = []
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
-    
-    def move_left(self,keyboard_events): #Movimiento a la derecha
+
+    def draw_bullets(self, screen):
+        for bullet in self.bullets:
+            screen.blit(bullet.image, bullet.rect)
+    def move_left(self, keyboard_events):
         if keyboard_events[pygame.K_LEFT]:
             self.rect.x -= self.speed
-    
-    def move_right(self,keyboard_events): #Movimiento a la izquierda
+
+    def move_right(self, keyboard_events):
         if keyboard_events[pygame.K_RIGHT]:
             self.rect.x += self.speed
-    
-    def move_up(self,keyboard_events): #Movimiento hacia arriba
+
+    def move_up(self, keyboard_events):
         if keyboard_events[pygame.K_UP]:
             self.rect.y -= self.speed
-    
-    def move_down(self,keyboard_events): #Movimiento hacia abajo
+
+    def move_down(self, keyboard_events):
         if keyboard_events[pygame.K_DOWN]:
             self.rect.y += self.speed
 
-    def update(self,keyboard_events):
-        self.move_up(keyboard_events)   
+    def fire_bullet(self):
+
+        bullet = Bullet((self.rect.x + self.rect.width // 2, self.rect.y))  
+        self.bullets.append(bullet)
+
+    def update_bullets(self):
+        for bullet in self.bullets:
+            bullet.update()
+            if bullet.rect.y < 0:
+                self.bullets.remove(bullet)
+
+    def update(self, keyboard_events):
+        self.move_up(keyboard_events)
         self.move_down(keyboard_events)
         self.move_left(keyboard_events)
         self.move_right(keyboard_events)
-        #Limita el espacio de movimiento en el eje x al tamaño de la pantalla
-        self.rect.x = max(0, min(self.rect.x, SCREEN_WIDTH - self.rect.width))  
-        #Limita el espacio de movimiento en el eje y al tamaño de la pantalla
+        self.rect.x = max(0, min(self.rect.x, SCREEN_WIDTH - self.rect.width))
         self.rect.y = max(0, min(self.rect.y, SCREEN_HEIGHT - self.rect.height))
+        self.update_bullets()
