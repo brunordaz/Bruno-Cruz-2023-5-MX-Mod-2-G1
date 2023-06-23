3e71980b36fa8a89c75301468a652d19ead383bb
brunordaz <126126201+brunordaz@users.noreply.github.com>
2023-06-22 19:07:12 -0600

    Update TAREA FINAL

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -1,12 +1,13 @@
 import pygame
-
 from pygame.sprite import Sprite
-from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, BULLET
+from game.components.shield import Shield
+from game.utils.constants import SPACESHIP, SCREEN_WIDTH, SCREEN_HEIGHT, BULLET, SPACESHIP_SHIELD
+
 
 class Bullet(Sprite):
     def __init__(self, position):
         super().__init__()
-        self.image = pygame.transform.scale(BULLET, (30, 30))
+        self.image = pygame.transform.scale(BULLET, (20, 20))
         self.rect = self.image.get_rect()
         self.rect.x = position[0]
         self.rect.y = position[1]
@@ -14,15 +15,19 @@ class Bullet(Sprite):
     def update(self):
         self.rect.y -= 8
 
+
 class Spaceship(Sprite):
     def __init__(self):
+        super().__init__()
         self.image = SPACESHIP
         self.image = pygame.transform.scale(self.image, (50, 60))
         self.rect = self.image.get_rect()
-        self.speed = 9
+        self.speed = 8
         self.rect.x = (SCREEN_WIDTH - self.rect.width) // 2
         self.rect.y = SCREEN_HEIGHT - self.rect.height
         self.bullets = []
+        self.shield = Shield()
+        self.shield_active = False
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
@@ -30,6 +35,7 @@ class Spaceship(Sprite):
     def draw_bullets(self, screen):
         for bullet in self.bullets:
             screen.blit(bullet.image, bullet.rect)
+
     def move_left(self, keyboard_events):
         if keyboard_events[pygame.K_LEFT]:
             self.rect.x -= self.speed
@@ -47,9 +53,14 @@ class Spaceship(Sprite):
             self.rect.y += self.speed
 
     def fire_bullet(self):
-
-        bullet = Bullet((self.rect.x + self.rect.width // 2, self.rect.y))  
+        bullet = Bullet((self.rect.x + self.rect.width // 2, self.rect.y))
         self.bullets.append(bullet)
+
+    def reset(self):
+        self.rect.centerx = SCREEN_WIDTH // 2
+        self.rect.bottom = SCREEN_HEIGHT - 10
+        self.speed_x = 0
+        self.bullets.clear()
 
     def update_bullets(self):
         for bullet in self.bullets:
@@ -65,3 +76,10 @@ class Spaceship(Sprite):
         self.rect.x = max(0, min(self.rect.x, SCREEN_WIDTH - self.rect.width))
         self.rect.y = max(0, min(self.rect.y, SCREEN_HEIGHT - self.rect.height))
         self.update_bullets()
+
+    def shield_collision(self):
+        self.image = pygame.transform.scale(SPACESHIP_SHIELD, (50, 60))
+        self.shield.kill()
+
+    def normal(self):
+        self.image = pygame.transform.scale(SPACESHIP, (50, 60))
